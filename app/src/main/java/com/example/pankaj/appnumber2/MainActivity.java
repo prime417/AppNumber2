49fe78c14ee6c98c3e3c3797c2e671286679c7f4
Pankaj Salunkhe <prime417@.github.com>
2018-01-02 10:35:55 +0530

    Initial commit

---
+++ app/src/main/java/com/example/pankaj/appnumber2/MainActivity.java
@@ -0,0 +1,71 @@
+package com.example.pankaj.appnumber2;
+
+import android.os.Bundle;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.Snackbar;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.View;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.widget.TextView;
+
+import java.util.StringTokenizer;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        Toolbar toolbar =  findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+        TextView txt1 = findViewById(R.id.txt1);
+        TextView txt2 = findViewById(R.id.txt2);
+        TextView txt3 = findViewById(R.id.txt3);
+
+      /*  txt1.setText("My Own Text");
+
+        String str1 = "some value";
+
+        StringTokenizer strObject1 = new StringTokenizer(str1);
+
+        txt2.setText(strObject1.nextToken());
+        txt1.setText(strObject1.nextToken());
+        */
+
+      String str2 = "This is another text@this is your text@this is my text";
+
+      StringTokenizer strObject2 = new StringTokenizer(str2,"@");
+
+      txt1.setText(strObject2.nextToken());
+
+      txt2.setText(strObject2.nextToken());
+
+      txt3.setText(strObject2.nextToken() + " fine");
+
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+}
